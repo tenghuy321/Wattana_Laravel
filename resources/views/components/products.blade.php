b4e3ff0ff316f2b509156e4b1de2ce44826d8378
Tenghuy <tenghuyly35@gmailcom>
2025-06-30 14:53:31 +0700

    updated

--- resources/views/components/products.blade.php
+++ resources/views/components/products.blade.php
@@ -83,7 +83,7 @@
     <div
         class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 max-w-7xl mx-auto px-4 justify-items-center">
         @foreach ($product_unique as $index => $item)
-            <div class="w-full text-center p-10 bg-white rounded shadow-md" data-aos="zoom-in-up"
+            <div class="w-full text-center p-2 sm:p-10 bg-white rounded shadow-md" data-aos="zoom-in-up"
                 data-aos-duration="1200">
                 <h1 class="text-[18px] text-[#FF3217] font-bold">
                     {{ $index + 1 < 10 ? '0' . ($index + 1) : $index + 1 }}
