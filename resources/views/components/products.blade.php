478cc68250c1091a086a27ebf753162a63dd7a34
Tenghuy <tenghuyly35@gmailcom>
2025-06-30 15:31:24 +0700

    updated

--- resources/views/components/products.blade.php
+++ resources/views/components/products.blade.php
@@ -110,7 +110,7 @@
             </svg>
         </button>
 
-        <button
+        {{-- <button
             class="hidden md:flex absolute left-4 top-1/2 transform -translate-y-1/2 text-[12px] text-[#fff] p-2 w-12 h-12 z-50 transition-transform duration-200 hover:scale-110"
             onclick="navigateLightbox(-1)">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
@@ -126,7 +126,7 @@
                 stroke="currentColor" class="size-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
-        </button>
+        </button> --}}
 
         <img id="lightbox-image"
             class="w-full h-full object-contain object-center rounded-sm transform transition-transform duration-300"
